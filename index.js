76a9880413b4a175f88e7fbdc591435039c6a98e
zhuanglunzhi <zhuanglunzhi@followme.cn>
2019-02-14 16:40:28 +0800

    add webToImg

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const cp = require('child_process');
 const path = require('path');
 const phantomjsPath = path.join(__dirname, 'src', 'phantomjs', 'webconvert.js');
 
+/**
+ * 将网页转成pdf
+ * @param {string} web 网页地址 https://www.baidu.com
+ * @param {string} pdf 文件输出路劲 ~/files/a.pdf
+ */
 function webToPdf(web, pdf) {
     const phantomjs = 'phantomjs';
     const format = 'pdf';
@@ -16,13 +21,31 @@ function webToPdf(web, pdf) {
         quality,
         timeout
     ];
-    console.info(cmd.join(' '));
+    // console.log(cmd.join(' '));
     cp.execSync(cmd.join(' '));
 }
 
-// todo
-async function webToImg(web, img) {
-
+/**
+ * 将指定网页转成图片
+ * @param {string} web 网页地址 https://www.baidu.com
+ * @param {string} img 文件输出路劲 ~/files/a.jpg
+ */
+function webToImg(web, img) {
+    const phantomjs = 'phantomjs';
+    const quality = 100;
+    const timeout = 10;
+    const format = path.extname(img).substr(1);
+    const cmd = [
+        phantomjs,
+        phantomjsPath,
+        web,
+        img,
+        format,
+        quality,
+        timeout
+    ];
+    console.log(cmd.join(' '));
+    cp.execSync(cmd.join(' '));
 }
 
 // todo
--- test/index.js
+++ test/index.js
@@ -1,15 +1,27 @@
 const path = require('path');
+const fs = require('fs');
 const o = require('../index');
 
 // ------------------------- 测试webToPdf -------------------------
-const web = 'https://www.baidu.com/';
-const pdf = path.join(__dirname, '..', 'files', 'a.pdf');
-try {
-    o.webToPdf(web, pdf);
-    console.log('done');
-} catch (err) {
-    console.error(err);
-}
+// const web = 'https://www.baidu.com/';
+// const pdf = path.join(__dirname, '..', 'files', 'a.pdf');
+// try {
+//     o.webToPdf(web, pdf);
+//     console.log('done');
+// } catch (err) {
+//     console.error(err);
+// }
 // ------------------------- 测试webToPdf -------------------------
 
 
+// ------------------------- 测试webToImg -------------------------
+const web = 'https://www.baidu.com/';
+const img = path.join(__dirname, '..', 'files', 'a.png');
+try {
+    o.webToImg(web, img);
+    console.log('done');
+    console.log(fs.existsSync(img));
+} catch (err) {
+    console.error(err);
+}
+// ------------------------- 测试webToImg -------------------------
